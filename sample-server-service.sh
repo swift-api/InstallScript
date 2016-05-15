842fda1f20ed6d74869c4fbd9542a4df6be4429b
Jakub Tomanik <j.tomanik@gmail.com>
2016-05-15 14:54:55 +0200

    starting sample-server

--- sample-server-service.sh
+++ sample-server-service.sh
@@ -3,6 +3,9 @@
 # This script is for creating a service file that will launch a Sample server
 # NOTE: this script depends on install-kitura.sh
 
+echo "creating sample-server.service"
+cd $HOME
+
 cat >$HOME/sample-server.service << EOF
 [Unit]
 Description=Sample Kitura Server
@@ -14,3 +17,8 @@ Restart=on-abort
 [Install]
 WantedBy=multi-user.target
 EOF
+
+echo "starting sample-server.service"
+cp sample-server.service /etc/systemd/system
+systemctl enable sample-server.service
+systemctl start sample-server.service
